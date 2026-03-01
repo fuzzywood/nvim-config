27de04346174460aae5893097a2c150541a30dba
spizzl <florin@woeginger.de>
2026-03-01 23:58:51 +0100

    smull

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,7 +7,7 @@ map("n", "<C-a>", "gg<S-v>G", { desc = "Select all", noremap = true })
 -- Buffers
 map("n", "<Tab>", ":BufferNext<CR>", { desc = "Move to next tab", noremap = true })
 map("n", "<S-Tab>", ":BufferPrevious<CR>", { desc = "Move to previous tab", noremap = true })
-map("n", "<leader>x", ":BufferClose<CR>", { desc = "Close Buffer", noremap = true })
+map("n", "<leader>x", ":BufferClose<CR>", { desc = "Close Buffer and Window", noremap = true })
 map("n", "<C-U>", ":redo", { desc = "Redo", noremap = true })
 
 -- Flash
